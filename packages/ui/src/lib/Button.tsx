cfdad30b911058db0059aa2104b637c92cb8d4aa
Bruno Paulino <bruno.paulino@n26.com>
2022-09-23 07:20:29 +0200

    Change button color

--- packages/ui/src/lib/Button.tsx
+++ packages/ui/src/lib/Button.tsx
@@ -6,7 +6,7 @@ const style = {
   height: "2.81rem",
   lineHeight: "2.8rem",
   borderRadius: "7px",
-  backgroundColor: "darkred",
+  backgroundColor: "#333",
   color: "white",
   border: "none",
 };
